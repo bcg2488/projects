ad6ee4495a73e3374fb022ab6255271fd852a474
bcg2488 <bcg2488@mavs.uta.edu>
2026-03-27 19:50:22 -0500

    UVS Projects

---
+++ UVS/Vehicle Simulation/UGVparameters.m
@@ -0,0 +1,17 @@
+%Parameter definition for our UGV model
+rL=0.052959;    %Radius of left wheel [m]
+rR=0.052959;    %Radius of right wheel [m]
+b=0.5842;       %Effective width of UGV [m]
+
+Vmax=0.44;      %Max translational speed [m/s]
+wmax=Vmax/rR;   %Max angular speed [rad/s]
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%% Way Points %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+X_array  = [0 5 0 5]; 
+Y_array  = [5 5 0 0]; 
+
+%%%%%%%%%%%%%%%%%%% guidance parameter %%%%%%%%%%%%%%%%%%%%%%%%%%%
+rp1 =  0.3; % 1 -> [m] proximity circle to start slowing down
+rp2 =  0.5;  % [m] radius of wayPoint proximity circle to switch to the next wayPoint
